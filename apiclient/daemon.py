4456851a35b0c1f90784c4bb33715f5bc2eb0208
A.Sikorski <sikorski.aps@gmail.com>
2022-02-23 00:04:53 +0100

    task-1 | developing client

---
+++ apiclient/daemon.py
@@ -0,0 +1,11 @@
+from time import sleep
+from os import environ
+
+import requests
+
+HBEAT_URL = environ.get("HBEAT_URL")
+HBEAT_TOKEN = environ.get("HBEAT_TOKEN")
+
+while True:
+    sleep(5)
+    requests.post(f'{HBEAT_URL}/?token={HBEAT_TOKEN}')
