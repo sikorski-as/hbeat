81c303231438122aa11c51e8ddb9195098356d3d
A.Sikorski <sikorski.aps@gmail.com>
2022-02-23 21:15:17 +0100

    task-1 | changed url from devices/ to device/

--- apiserver/urls.py
+++ apiserver/urls.py
@@ -19,6 +19,5 @@ from apiapp import views as api_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/v1/entities/', api_views.AllExampleEntities.as_view()),
-    path('api/v1/devices/<int:pk>', api_views.DeviceDetail.as_view()),
+    path('api/v1/device/<int:pk>', api_views.DeviceDetail.as_view())
 ]
